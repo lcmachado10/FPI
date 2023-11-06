ebac605eecda3975fc5cc40e79fda9c914c739a0
lcmachado16 <lucascmac@hotmail.com>
2023-11-06 11:12:59 -0300

    first commit. cvui not working but opencv ok

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+#include "operations.h"
+
 using namespace cv;
 using namespace std;
+
+
+
 
 int main() {
 
@@ -11,8 +16,6 @@ int main() {
 
     std::string src = "/Users/lucasmachado/CIC/FPI/test_images/";
     std::string dst = "/Users/lucasmachado/CIC/FPI/destiny_images/";
-
-
 
     std::string src_gramado = src + "Gramado_72k.jpg";
     string dst_gramado = dst + "Gramado_72k.jpg";
@@ -24,42 +27,63 @@ int main() {
         return -1;
     }
 
-    int height = img.rows;
-    int width = img.cols;
+    // Crie uma matriz 5x5 preenchida com zeros
+    cv::Mat m1(5, 5, CV_8UC1, cv::Scalar(0));
 
-    cv::Mat mirrored_img = img.clone();  // Criar uma cópia da imagem
-
-    for (int row = 0; row < height / 2; row++) {
-        int opposite_row = height - 1 - row;
-        uchar* row_ptr = img.ptr<uchar>(row);
-        uchar* opposite_row_ptr = img.ptr<uchar>(opposite_row);
-
-        // Inverta as linhas usando memcpy
-        std::memcpy(mirrored_img.data + row * mirrored_img.step, opposite_row_ptr, width * mirrored_img.elemSize());
-        std::memcpy(mirrored_img.data + opposite_row * mirrored_img.step, row_ptr, width * mirrored_img.elemSize());
+    for(int i=0; i < 5; i++){
+        for(int j=0; j < 5; j++){
+            m1.at<uchar>(i, j) = j*(i+1);
+        }
     }
 
+    cv::Mat m2(5, 5, CV_8UC1, cv::Scalar(0));
+
+    int columns = m1.cols;
+
+    // Selecione a segunda linha (índice 1) da matriz m1
+    cv::Mat linha = m1.row(0);
+
+
+//    for (int j = 0; j < columns; j++) {
+//        cv::Mat column = m1.col(j);
+//        memcpy(m2.col(columns - j - 1).data, column.data, m1.rows);
+//    }
+    cv::Mat column = m1.col(4);
+
+    std::cout << "Column :\n" << column << std::endl;
+
+    memcpy(m2.col(0).data, column.data, m1.rows);
+
+    // Imprima a matriz m2
+    std::cout << "Matriz m1:\n" << m1 << std::endl;
+
+    // Imprima a matriz m2
+    std::cout << "Matriz m2:\n" << m2 << std::endl;
+    // Transponha a matriz
+    cv::Mat transposed = m1.t();
+
+    // Exiba a matriz original e a matriz transposta
+    std::cout << "Matriz Original:\n" << m1 << std::endl;
+    //std::cout << "Matriz Transposta:\n" << transposed << std::endl;
+
+    cv::Mat m3 = vflip(transposed);
+    cv::Mat m4 =  m3.t();
+    std::cout << "M3 :\n" << m4 << std::endl;
+
+
+
+    cv::Mat img_vf = vflip(img);
+    cv::Mat img_hf = hflip(img);
+
+    // Exiba a imagem original e a imagem invertida
     cv::imshow("Imagem Original", img);
-    cv::imshow("Imagem Espelhada Verticalmente", mirrored_img);
+    cv::imshow("Imagem VF", img_vf);
+    cv::imshow("Imagem HF", img_hf);
+    cv::waitKey(0);
 
-    cout << "lucas " << endl;
 
-    bool isSuccess = imwrite(dst_gramado, img); //write the image to a file as JPEG
-    //bool isSuccess = imwrite("D:/MyImage.png", image); //write the image to a file as PNG
-    if (isSuccess == false)
-    {
-        cout << "Failed to save the image" << endl;
-        cin.get(); //wait for a key press
-        return -1;
-    }
+    return 0;
 
-    String windowName = "The Saved Image"; //Name of the window
-    namedWindow(windowName); // Create a window
-    imshow(windowName, img); // Show our image inside the created window.
-
-    waitKey(0); // Wait for any keystroke in the window
-
-    destroyWindow(windowName); //destroy the created window
 
     cv::waitKey(0);
 
---
+++ operations.cpp
@@ -0,0 +1,84 @@
+//
+// Created by Lucas Machado on 06/11/23.
+//
+
+#include "operations.h"
+/*
+ *  convert_rgb_to_gray: Mat3b -> Mat1b
+ *      Converte imagem colorida[Mat3b] para imagem em tom de cinza [Mat1b] (luminancia)
+ *          Formula utilizada para o calculo:
+ *          L = 0.299 * Red + 0.587 * Green + 0.114 * Blue
+ */
+
+cv::Mat1b convert_rgb_to_gray(const Mat3b& src) {
+    cv::Mat1b dst;
+    dst = Mat1b(src.rows, src.cols);
+
+    for (int j = 0; j < src.rows; j++) {
+        for (int i = 0; i < src.cols; i++) {
+            Vec3b color = src.at<Vec3b>(j, i);
+            uchar gray_value = static_cast<uchar>(0.114 * color[0] + 0.587 * color[1] + 0.299 * color[2]);
+            dst.at<uchar>(j, i) = gray_value;
+        }
+    }
+    return dst;
+}
+
+/* vertical_flip: espelhamento vertical de imagens coloridas,
+ *                  trocando pixel a pixel atraves de dois lacos for
+ */
+cv::Mat3b vertical_flip(cv::Mat3b src) {
+    cv::Mat3b dst(src.rows, src.cols);
+    int columns = src.cols;
+    int rows = src.rows;
+
+    for (int j = 0; j < columns; j++) {
+        for (int i = 0; i < rows; i++) {
+            cv::Vec3b color = src.at<cv::Vec3b>(i, j);
+            int mirrored_row = rows - i - 1; // Calcula posição espelhada na matriz de destino
+            memcpy(&dst.at<cv::Vec3b>(mirrored_row, j), &color, sizeof(cv::Vec3b));
+        }
+    }
+    return dst;
+}
+
+/* horizontal_flip: espelhamento vertical de imagens coloridas,
+ *                  trocando pixel a pixel atraves de dois lacos for
+ */
+cv::Mat3b horizontal_flip(cv::Mat3b src) {
+    int rows = src.rows;
+    int cols = src.cols;
+    cv::Mat3b dst(rows, cols);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cv::Vec3b color = src.at<cv::Vec3b>(i, j);
+            int mirrored_col = cols - j - 1;
+            memcpy(&dst.at<cv::Vec3b>(i, mirrored_col), &color, sizeof(cv::Vec3b));
+        }
+    }
+    return dst;
+}
+/*
+ * vflip: Espelhamento vertical da imagem,
+ *        utilizando memcpy para inverter linha a linha em vez de ponto a ponto
+ */
+cv::Mat vflip(const cv::Mat& src) {
+    int columns = src.cols;
+    int rows = src.rows;
+    cv::Mat dst(rows, columns, src.type());
+
+    for (int i = 0; i < rows; i++) {
+        cv::Mat row = src.row(i);
+        std::memcpy(dst.row(rows - i - 1).data, row.data, src.cols * src.elemSize());
+    }
+
+    return dst;
+}
+//hflip: Espelhamento horizontal da imagem,
+//                transpoe a matriz, e utiliza a funcao vflip, em seguida transpoe de volta
+cv::Mat hflip(const cv::Mat& src) {
+    cv::Mat img_aux = src.t();
+    cv::Mat dst = vflip(img_aux).t();
+    return dst;
+}
